bbf188ec66d63e0375644a61fe4579fef78a97c0
Thiago Falcao <124840137+thiagofalcaosenac@users.noreply.github.com>
2023-09-25 21:02:00 -0300

    New Update PasswordService
    New Update PasswordService

--- PasswordService.php
+++ PasswordService.php
@@ -1,10 +1,34 @@
-use Defuse\Crypto\KeyOrPassword;
+<?php
+    echo 'isto é uma string comum';
 
-function createKey() {
-    $password = "exemplo";
-    return KeyOrPassword::createFromPassword($password); // Noncompliant
-}
+    echo 'Você pode incluir novas linhas em strings,
+    dessa maneira que estará
+    tudo bem';
 
+    // Imprime: Arnold disse uma vez: "I'll be back"
+    echo 'Arnold disse uma vez: "I\'ll be back"';
 
-$filename = $_GET["filename"];
-include $filename; // Noncompliant
+    // Imprime: Você tem certeza em apagar C:\*.*?
+    echo 'Você tem certeza em apagar C:\\*.*?';
+
+    // Imprime: Você tem certeza em apagar C:\*.*?
+    echo 'Você tem certeza em apagar C:\*.*?';
+
+    // Imprime: Isto não será substituido: \n uma nova linha
+    echo 'Isto não será substituido: \n uma nova linha';
+
+    // Imprime: Variáveis $também não $expandem
+    echo 'Variáveis $também não $expandem';
+
+    use Defuse\Crypto\KeyOrPassword;
+
+    function createKey()
+    {
+        $password = "exemplo";
+        return KeyOrPassword::createFromPassword($password); // Noncompliant
+    }
+
+    $filename = $_GET["filename"];
+    include $filename; // Noncompliant
+
+?>
